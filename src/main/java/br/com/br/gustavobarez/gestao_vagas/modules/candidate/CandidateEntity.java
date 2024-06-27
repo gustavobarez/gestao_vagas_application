414666453fe782900d0cd1e4323d5b2d85372544
Barez <gbarez44@gmail.com>
2024-06-27 13:39:40 -0300

    Commit inicial

---
+++ src/main/java/br/com/br/gustavobarez/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -0,0 +1,40 @@
+package br.com.br.gustavobarez.gestao_vagas.modules.candidate;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data // criar getters e setters para todos automaticamente
+@Entity(name = "candidate")
+public class CandidateEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+
+    @Pattern(regexp = "\\S+", message = "Nao pode contar espacos no campo [username]")
+    private String username;
+
+    @Email(message = "O campo [email] deve conter um e-mail valido")
+    private String email;
+
+    @Length(min = 10, max = 100)
+    private String password;
+    private String description;
+    private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
